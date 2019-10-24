ad185f00f31a5b92a7efe6c97152d04a8a957e22
passeroe <passeroe@vt.edu>
2019-10-24 12:13:48 -0600

    Building a flow scenario

--- flow.scenario.R
+++ flow.scenario.R
@@ -1,35 +1,40 @@
 # This function generates flow scenarios from historic flow record
-# Last edited by Elaina Passero on 10/17/19
+# Last edited by Elaina Passero on 10/24/19
 
-flow.scenario <- function(hydrograph,enforce_med,per_red,median_q,withdrawal){
+flow.scenario <- function(hydrograph,enforce_med,median_q,withdrawal,per_red){
   if(withdrawal > 0){ # fixed withdrawal
-    if(enforceMed == "Yes"){
+    if(enforce_med == "Yes"){
       for(r in 1:length(hydrograph$date)){
-        if(hydrograph$discharge[[r]] > medianQ){
-          hydrograph$medERevQ[[r]] <- hydrograph$discharge[[r]]-withdrawal
+        if(hydrograph$discharge[[r]] > median_q){
+          hydrograph$revised_q[[r]] <- hydrograph$discharge[[r]]-withdrawal
         } else{
-          hydrograph$medERevQ[[r]] <- hydrograph$discharge[[r]]
+          hydrograph$revised_q[[r]] <- hydrograph$discharge[[r]]
         }
       }
     } else{
-      hydrograph$subQ <- hydrograph$discharge-withdrawal
+      hydrograph$revised_q <- hydrograph$discharge-withdrawal
     }
   }
   
   if(per_red > 0){ # percent withdrawal
-    if(enforceMed == "Yes"){
+    if(enforce_med == "Yes"){
       for(s in 1:length(hydrograph$date)){
-        if(hydrograph$discharge[[s]] > medianQ){
-          hydrograph$revisedQ[[s]] <- hydrograph$discharge[[s]]*(1-perRed)
+        if(hydrograph$discharge[[s]] > median_q){
+          hydrograph$revised_q[[s]] <- hydrograph$discharge[[s]]*(1-per_red)
         } else{
-          hydrograph$revisedQ[[s]] <- hydrograph$discharge[[s]]
+          hydrograph$revised_q[[s]] <- hydrograph$discharge[[s]]
         }
       }
     } else{
-      hydrograph$revisedQ <- hydrograph$discharge*(1-perRed)
+      hydrograph$revised_q <- hydrograph$discharge*(1-per_red)
     }
   }
   
+  hydrograph <- hydrograph %>%
+    select(-discharge) %>%
+    rename(discharge = revised_q)
+  
+return(hydrograph)
 }
 
 
